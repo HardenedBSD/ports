63748d31431752472ec317e571550518c433c5ff
Shawn Webb <shawn.webb@hardenedbsd.org>
2021-05-20 12:39:06 -0400

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- Mk/Uses/go.mk
+++ Mk/Uses/go.mk
@@ -82,13 +82,10 @@ GO_PKGNAME=	${PORTNAME}
 GO_TARGET?=	${GO_PKGNAME}
 GO_TESTTARGET?=	./...
 
-<<<<<<< HEAD
 .if empty(GO_BUILDFLAGS:M-buildmode*)
 GO_BUILDFLAGS+=-v -buildmode=exe
 .endif
-=======
-GO_BUILDFLAGS+=	-v -buildmode=exe -trimpath
->>>>>>> origin/freebsd/main
+GO_BUILDFLAGS+=	-trimpath
 .if !defined(WITH_DEBUG) && empty(GO_BUILDFLAGS:M-ldflags*)
 GO_BUILDFLAGS+=	-ldflags=-s
 .endif
